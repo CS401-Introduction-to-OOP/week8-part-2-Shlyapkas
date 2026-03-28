09f4743c897799d10d45edc774842afc841676fa
Shlyapkas <149323530+Shlyapkas@users.noreply.github.com>
2026-03-28 16:09:44 +0200

    Stage2

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,10 @@
+﻿public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;  // boxing. A change from reference into value(object)
+        int c = (int)b; // unboxing. A change from value(object) into reference
+        Console.WriteLine($"a: {a}, b: {b}, c: {c}"); // printing
+    }
+}
