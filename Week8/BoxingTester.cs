4423d6f91f7edf116f90c601f0012b5153484d72
Shlyapkas <149323530+Shlyapkas@users.noreply.github.com>
2026-03-28 16:18:37 +0200

    Stage3

--- Week8/BoxingTester.cs
+++ Week8/BoxingTester.cs
@@ -3,7 +3,7 @@
     public static void Test()
     {
         int a = 10;
-        object b = a;  // boxing. A change from reference into value(object)
+        object b = a;  // boxing. A change from reference into value(object). Makes a new object in heap
         int c = (int)b; // unboxing. A change from value(object) into reference
         Console.WriteLine($"a: {a}, b: {b}, c: {c}"); // printing
     }
