f9a2c8895771bbf4e8d79d136034dfe7f2fa6a87
Shlyapkas <149323530+Shlyapkas@users.noreply.github.com>
2026-03-28 15:46:55 +0200

    Stage3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -3,7 +3,21 @@ class Program
 {
     public static void Main()
     {
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        var manager = new ResourceManager<Resource>();
+        manager.Add(file);
+        manager.Add(network);
         
+        manager.OpenAll();
+        
+        using(var res = new FileResource("file"))
+        {
+            res.Open();
+        }
+        
+        manager.CloseAll();
+        Console.WriteLine("Done.");
     }
     
 }
