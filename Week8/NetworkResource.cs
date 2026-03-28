90247c3751198beaaa2edb49cb2af34747b0a4a1
Shlyapkas <149323530+Shlyapkas@users.noreply.github.com>
2026-03-28 15:35:19 +0200

    Class NetworkResource

---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,19 @@
+﻿public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+    }
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine("Diagnostic message");
+    }
+}
